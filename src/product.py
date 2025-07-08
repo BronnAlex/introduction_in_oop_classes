c9bbe2c94df1f44b7f5ebce201d364be29a10310
Aleksey Bronnikov <lesha.bronnikov.99@mail.ru>
2025-07-08 15:21:18 +0300

    Создал файл .flake8 добавил  в него конфигурацию, также конфигурацию в pyproject.toml и установил линтеры

--- src/product.py
+++ src/product.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     print(category1.category_count)
     print(category1.product_count)
 
-    product4 = Product("55 \"QLED 4K\"", "Фоновая подсветка", 123000.0, 7)
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
     category2 = Category("Телевизоры",
                          "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
                          [product4])
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     print(category2.name)
     print(category2.description)
     print(len(category2.products))
-    print(category2.products[0].name)
+    print(category2.products)
 
     print(Category.category_count)
     print(Category.product_count)
