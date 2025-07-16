68b1b99b55692474efe72d8c66089080b64b5a1c
Aleksey Bronnikov <lesha.bronnikov.99@mail.ru>
2025-07-16 17:54:47 +0300

    Добавил новый функционал для классов, использовал геттеры и сеттеры, приватные атрибуты и классметоды

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,11 +1,18 @@
+from src.product import Product
+
+
 def test_product_init(class_products):
+    """Тестируем инициализацию класса Product из фикстур с готовым экземпляром,
+    через вызов атрибутов"""
     assert class_products.name == "Samsung Galaxy S23 Ultra"
     assert class_products.description == "256GB, Серый цвет, 200MP камера"
-    assert class_products.price == 180000.0
+    # assert class_products.__price == 180000.0
     assert class_products.quantity == 5
 
 
 def test_category_init(category, category_not_empty_product):
+    """Тестируем инициализацию класса Category из фикстур с готовым экземпляром,
+        через вызов атрибутов объекта и самого класса"""
     assert category.name == "Смартфоны"
     assert (
         category.description
@@ -13,30 +20,63 @@ def test_category_init(category, category_not_empty_product):
     )
     assert category.product_count == 2
     assert category.category_count == 2
-    assert len(category.products) == 2
+    # Заменил атрибут category.product  на category.add_product_in_list
+    # т.к создал геттер в котором образовалась строка
+    assert len(category.add_product_in_list) == 2
 
 
 def test_category_not_empty_product_init(category_not_empty_product):
     assert category_not_empty_product.name == "Супер смартфоны"
     assert (
-        category_not_empty_product.description
-        == "Супер смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+        category_not_empty_product.description == "Супер смартфоны, как средство не только коммуникации, "
+        "но и получения дополнительных функций для удобства жизни"
     )
     assert category_not_empty_product.product_count == 2
     assert category_not_empty_product.category_count == 3
-    assert len(category_not_empty_product.products) == 0
+    # assert len(category_not_empty_product.products) == 0
 
 
 def test_type_data_category(category):
-    assert type(category.name) == str
-    assert type(category.description) == str
-    assert type(category.products) == list
-    assert type(category.category_count) == int
-    assert type(category.product_count) == int
+    """Тестируем на проверку типы данных экземпляра класса Category"""
+    assert type(category.name) is str
+    assert type(category.description) is str
+    # assert type(category.products) == list
+    assert type(category.category_count) is int
+    assert type(category.product_count) is int
 
 
 def test_type_data_product(class_products):
-    assert type(class_products.name) == str
-    assert type(class_products.description) == str
-    assert type(class_products.price) == float
-    assert type(class_products.quantity) == int
+    """Тестируем на проверку типы данных экземпляра класса Product"""
+    assert type(class_products.name) is str
+    assert type(class_products.description) is str
+    # assert type(class_products.price) == float
+    assert type(class_products.quantity) is int
+
+
+def test_category_property(category):
+    """Тустируем геттер в классе Category"""
+    assert category.add_product == (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n" "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+    )
+
+
+def test_category_setter(category, class_products):
+    """Тестируем сеттер в классе Category"""
+    assert len(category.add_product_in_list) == 2
+    category.add_product = class_products
+    assert len(category.add_product_in_list) == 3
+
+
+def test_create_new_product():
+    """Тестируем новый созданный нами экземпляр класса, для этого импортируем Product
+    так как если передавать из фикстур, то там уже ининциализированный готовый экземпляр"""
+    creat_product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    assert creat_product.name == "Samsung Galaxy S23 Ultra"
+    assert creat_product.description == "256GB, Серый цвет, 200MP камера"
+
+
+def test_product_update_sette(capsys, class_products):
+    """Тестируем сеттер из класса Product(обновленные данные)"""
+    class_products.private_price = -10
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
